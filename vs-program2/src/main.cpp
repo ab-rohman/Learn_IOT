f260c688cd4dc032b9601dcdc328bdcbe25ed295
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-03-23 02:51:18 +0700

    Done Task 1

--- vs-program2/src/main.cpp
+++ vs-program2/src/main.cpp
@@ -2,13 +2,16 @@
 
 #define RED_LED D5   //led warna merah
 #define GREEN_LED D6 //led warna hijau
+#define BOARD_LED1 D0 //led pada GPIO 16
+#define BOARD_LED2 D4 //led pada GPIO 2
 #define BLUE_LED D7  //led warnah biru
 
 void setup()
 {
   Serial.begin(115200);
-  pinMode(RED_LED, OUTPUT); //atur pin-pin digital sebagai output
-
+  pinMode(GREEN_LED, OUTPUT); //atur pin-pin digital sebagai output
+  pinMode(BOARD_LED1, OUTPUT); //atur pin-pin digital sebagai output
+  pinMode(BOARD_LED2, OUTPUT); //atur pin-pin digital sebagai output
   Serial.println("Contoh Program LED SOS");
 }
 
@@ -17,9 +20,9 @@ void loop()
   // 3 dits (3 titik atau huruf S)
   for (int x = 0; x < 3; x++)
   {
-    digitalWrite(RED_LED, HIGH); // LED nyala
+    digitalWrite(GREEN_LED, HIGH); // LED nyala
     delay(150);                  // delay selama 150ms
-    digitalWrite(RED_LED, LOW); // LED mati
+    digitalWrite(GREEN_LED, LOW); // LED mati
     delay(100);                  // delay selama 100ms
   }
   delay(100);
@@ -27,9 +30,9 @@ void loop()
   // 3 dahs (3 garis atau huruf O)
   for (int x = 0; x < 3; x++)
   {
-    digitalWrite(RED_LED, HIGH); // LED nyala
+    digitalWrite(BOARD_LED1, HIGH); // LED nyala
     delay(400);                  // delay selama 400ms
-    digitalWrite(RED_LED, LOW); // LED mati
+    digitalWrite(BOARD_LED1, LOW); // LED mati
     delay(100);                  // delay selama 100ms
   }
 
@@ -38,9 +41,9 @@ void loop()
   // 3 dits again (3 titik atau huruf S)
   for (int x = 0; x < 3; x++)
   {
-    digitalWrite(RED_LED, HIGH); // LED nyala
+    digitalWrite(BOARD_LED2, HIGH); // LED nyala
     delay(150);                  // delay selama 150ms
-    digitalWrite(RED_LED, LOW); // LED mati
+    digitalWrite(BOARD_LED2, LOW); // LED mati
     delay(100);                  // delay selama 100ms
   }
 
