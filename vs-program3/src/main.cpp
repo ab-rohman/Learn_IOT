73e00341f029f464ebfcb0d63bedf3194d61600d
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-04-01 20:35:49 +0700

    step 12 done

--- vs-program3/src/main.cpp
+++ vs-program3/src/main.cpp
@@ -1,9 +1,41 @@
 #include <Arduino.h>
+#include <DHT.h>
+
+#define DHTTYPE DHT11
+
+DHT dht(D1, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Menggunakan DHT11");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  delay(2000);
+  float h = dht.readHumidity();
+  float c = dht.readTemperature();
+  float f = dht.readTemperature(true);
+
+  if (isnan(h) || isnan(c) || isnan(f))
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(c, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(c);
+  Serial.print(F("°C "));
+  Serial.print(f);
+  Serial.print(F("°F  Heat index: "));
+  Serial.print(hic);
+  Serial.print(F("°C "));
+  Serial.print(hif);
+  Serial.println(F("°F"));
 }
