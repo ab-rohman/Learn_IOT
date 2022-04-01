941e3ef012c2fc17406886cd991202730e01d3a9
Abdul_Rohman <rokhman12abdul@gmail.com>
2022-04-01 21:25:53 +0700

    Task 2 Done

--- vs-program3/src/main.cpp
+++ vs-program3/src/main.cpp
@@ -2,13 +2,19 @@
 #include <DHT.h>
 
 #define DHTTYPE DHT11
+#define RED_LED D5 
+#define GREEN_LED D6 
+#define BLUE_LED D7
 
 DHT dht(D1, DHTTYPE);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Serial.println("Menggunakan DHT11");
+  pinMode(RED_LED,OUTPUT);
+  pinMode(GREEN_LED,OUTPUT);
+  pinMode(BLUE_LED,OUTPUT);
+  Serial.println("Menggunakan DHT11 untuk menyalakan lampu");
 }
 
 void loop() {
@@ -30,6 +36,22 @@ void loop() {
   float hif = dht.computeHeatIndex(f, h);
   float hic = dht.computeHeatIndex(c, h, false);
 
+  if ( c > 25 )
+  {
+    digitalWrite(RED_LED, HIGH); 
+    Serial.println("Kota berada pada suhu Panas "); 
+    delay(1000); 
+  } else if ( c > 19)
+  {
+    digitalWrite(BLUE_LED, HIGH); 
+    Serial.println("Kota berada pada suhu Normal "); 
+    delay(1000);  
+  } else { 
+    digitalWrite(GREEN_LED, HIGH); 
+    Serial.println("Kota berada pada suhu Dingin "); 
+    delay(1000); 
+  }
+  
   Serial.print(F("Humidity: "));
   Serial.print(h);
   Serial.print(F("%  Temperature: "));
@@ -45,4 +67,7 @@ void loop() {
   Serial.print(F("°C "));
   Serial.print(hif);
   Serial.println(F("°F"));
+  digitalWrite(GREEN_LED, LOW); 
+  digitalWrite(BLUE_LED, LOW); 
+  digitalWrite(RED_LED, LOW); 
 }
